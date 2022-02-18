f24276bd5eb63b80df7bd8c88ef75738d3cb494f
volkanyasiryilmaz <volkanyasiryilmaz@hotmail.com>
2022-02-18 11:49:57 +0300

    add to photo detail page

--- app.js
+++ app.js
@@ -1,28 +1,60 @@
 const express = require('express');
-const ejs=require("ejs");
-const path=  require('path');
+
+const mongoose = require('mongoose');
+
+const ejs = require('ejs');
+
+const path = require('path');
+
+const Photo = require('./models/Photo');
 
 const app = express();
 
-//TEMPLATE ENGINE
-
-app.set("view engine","ejs");
-
-//MIDDLEWARES
-app.use(express.static('public'))
-
-//ROUTES
-app.get('/', (req, res) => {
-  res.render("index")
+//connect DB
+mongoose.connect('mongodb://localhost/pcat', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.get('/about', (req,res)=> {
-  res.render("about")
-})
+//TEMPLATE ENGINE
 
-app.get('/add',(req,res)=>{
-  res.render("add")
-})
+app.set('view engine', 'ejs');
+
+//MIDDLEWARES
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//ROUTES
+app.get('/', async (req, res) => {
+  const photos =  await Photo.find({});
+  res.render('index', {
+    photos
+  });
+});
+
+app.get('/photos/:id', async (req, res) => {
+  // console.log(req.params.id);
+  // //res.render('about');
+  const photo= await Photo.findById(req.params.id);
+  res.render('photo',{
+    photo
+  })
+});
+
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
+});
 
 const port = 3000;
 app.listen(port, () => {
